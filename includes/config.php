e7f33da91b54572d5b8435bfc5270fd3a0763162
JoseJCanedo <canedo_jose@columbusstate.edu>
2019-08-25 23:00:48 +0000

    changed

--- includes/config.php
+++ includes/config.php
@@ -9,6 +9,7 @@ function config($key = '')
 {
     $config = [
         'name' => 'Simple PHP Website',
+        'pretty_uri' => true,
         'site_url' => 'https://csuphpdemo.herokuapp.com',
         'nav_menu' => [
             '' => 'Home',
@@ -20,5 +21,6 @@ function config($key = '')
         'content_path' => 'content',
         'version' => 'v3.0',
     ];
+    echo isset($config[$key]) ? $config[$key] : null;
     return isset($config[$key]) ? $config[$key] : null;
 }
--- includes/functions.php
+++ includes/functions.php
@@ -33,7 +33,7 @@ function nav_menu($sep = ' | ')
     $nav_items = config('nav_menu');
     foreach ($nav_items as $uri => $name) {
         $class = str_replace('page=', '', $_SERVER['QUERY_STRING']) == $uri ? ' active' : '';
-        $url = config('site_url') . '/' . '?page=' . $uri;
+        $url = config('site_url') . '/' . (config('pretty_uri') || $uri == '' ? '' : '?page=') . $uri;
         $nav_menu .= '<a href="' . $url . '" title="' . $name . '" class="item ' . $class . '">' . $name . '</a>' . $sep;
     }
     echo trim($nav_menu, $sep);
@@ -47,7 +47,6 @@ function nav_menu($sep = ' | ')
 function page_title()
 {
     $page = isset($_GET['page']) ? htmlspecialchars($_GET['page']) : 'Home';
-
     echo ucwords(str_replace('-', ' ', $page));
 }
 
@@ -59,7 +58,6 @@ function page_title()
 function page_content()
 {
     $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-
     $path = getcwd() . '/' . config('content_path') . '/' . $page . '.phtml';
 
     if (! file_exists($path)) {
